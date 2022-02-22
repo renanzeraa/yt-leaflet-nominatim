bded2885034dd38143aae53fb2dabe0ea5e6d434
renanzeraa <100175344+renanzeraa@users.noreply.github.com>
2022-02-21 22:49:16 -0300

    Update index.js

--- index.js
+++ index.js
@@ -1,9 +1,50 @@
+// const constmap = L.map('mapid').setView([-22.9193857,-47.1044369], 17);
+//////////////////////////////////////////////console.log(constmap)
+
+ //a funcao precisa carregar a view que eh a localizacao na tela. e tambem
+ //precisa carregar o marker que eh o ponto no mapa,devem estar sincronizados
+
+// L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
+//     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
+// }).addTo(constmap);
+
+//////////////////////////////////////////////L.marker([NUMERO_LAT, NUMERO_LAT]).addTo(map)
+// L.marker([-22.9193857,-47.1044369]).addTo(constmap)
+//     .bindPopup('A pretty CSS3 popup.<br> Easily customizable.')
+//     .openPopup();
+
+
+
+
+
+
+////////////////////////////////////////////// https://nominatim.openstreetmap.org/search.php?q=Rua+Ant%C3%B4nio+Rodrigues+Moreira+Neto+Campinas+Sao+Paulo+Brazil&format=jsonv2
+
+
+
+
+
+// constmap.on("click", pegaLatLng => {
+//     const {lat,lng} = pegaLatLng.latlng
+//     console.log(lat,lng)
+// })
+
+
+
+
+
+////////////////////////////////////////////////////////////
+
+//constante do botao pesquisar
 const searchInput = document.getElementById('search');
+//constante da lista de resultados
 const resultList = document.getElementById('result-list');
+//constante do elemento mapa
 const mapContainer = document.getElementById('map-container');
+//constante filhas dos
 const currentMarkers = [];
 
-const map = L.map(mapContainer).setView([20.13847, 1.40625], 2);
+const map = L.map(mapContainer).setView([-22.9193857,-47.1044369], 2);
 L.tileLayer('https://{s}.tile.openstreetmap.org/{z}/{x}/{y}.png', {
     attribution: '&copy; <a href="https://www.openstreetmap.org/copyright">OpenStreetMap</a> contributors'
 }).addTo(map);
@@ -22,7 +63,7 @@ function setResultList(parsedResult) {
     for (const marker of currentMarkers) {
         map.removeLayer(marker);
     }
-    map.flyTo(new L.LatLng(20.13847, 1.40625), 2);
+    map.flyTo(new L.LatLng(-22.9193857,-47.1044369), 2);
     for (const result of parsedResult) {
         const li = document.createElement('li');
         li.classList.add('list-group-item', 'list-group-item-action');
